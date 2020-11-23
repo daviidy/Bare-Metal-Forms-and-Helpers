279ab7daefb7c92799292216be913fa5b4c4f348
daviidy <yaodavidarmel@gmail.com>
2020-11-23 11:59:04 +0000

    editing and extra features completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to edit_user_path(@user)
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
